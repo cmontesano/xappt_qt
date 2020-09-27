9be86b2c62dc8aeb3e2e00ff7645b4981b6d1d9d
Christopher Montesano <c.e.montesano@gmail.com>
2020-09-26 22:32:56 -0400

    better loading of widgets + file edit

--- xappt_qt/gui/tool_page.py
+++ xappt_qt/gui/tool_page.py
@@ -4,6 +4,8 @@ from PySide2 import QtWidgets, QtCore
 
 import xappt
 
+from .widgets import *
+
 
 class ToolPage(QtWidgets.QWidget):
     def __init__(self, tool: xappt.BaseTool, parent: Optional[QtWidgets.QWidget] = None):
@@ -52,54 +54,97 @@ class ToolPage(QtWidgets.QWidget):
             return convert_fn(param)
         return QtWidgets.QWidget()
 
-    # noinspection DuplicatedCode
     def _convert_int(self, param: xappt.Parameter) -> QtWidgets.QWidget:
         if param.choices is not None:
-            w = QtWidgets.QComboBox()
-            w.addItems(param.choices)
-            if param.default is not None:
-                if isinstance(param.default, str):
-                    if param.default in param.choices:
-                        index = w.findText(param.default)
-                        w.setCurrentIndex(index)
-                elif isinstance(param.default, int):
-                    if 0 <= param.default < w.count():
-                        w.setCurrentIndex(param.default)
-            param.value = w.currentIndex()
-            w.currentIndexChanged[int].connect(lambda x: self.update_tool_param(param.name, x))
+            return self._convert_int_choice(param)
         else:
-            w = QtWidgets.QSpinBox(parent=self)
-            minimum = param.options.get("minimum", -999999999)
-            maximum = param.options.get("maximum", 999999999)
-            w.setMinimum(minimum)
-            w.setMaximum(maximum)
-            if param.default is not None:
-                w.setValue(param.default)
+            return self._convert_int_spin(param)
+
+    def _convert_int_choice(self, param: xappt.Parameter) -> QtWidgets.QWidget:
+        w = QtWidgets.QComboBox()
+        w.addItems(param.choices)
+        for v in (param.value, param.default):
+            if v is not None:
+                if isinstance(v, str):
+                    if v in param.choices:
+                        index = w.findText(v)
+                        w.setCurrentIndex(index)
+                elif isinstance(v, int):
+                    if 0 <= v < w.count():
+                        w.setCurrentIndex(v)
+                break
+        else:
+            param.value = w.currentIndex()
+        w.currentIndexChanged[int].connect(lambda x: self.update_tool_param(param.name, x))
+        return w
+
+    def _convert_int_spin(self, param: xappt.Parameter) -> QtWidgets.QWidget:
+        w = QtWidgets.QSpinBox(parent=self)
+        minimum = param.options.get("minimum", -999999999)
+        maximum = param.options.get("maximum", 999999999)
+        w.setMinimum(minimum)
+        w.setMaximum(maximum)
+        for v in (param.value, param.default):
+            if v is not None:
+                w.setValue(v)
+                break
+        else:
             param.value = w.value()
-            w.valueChanged[int].connect(lambda x: self.update_tool_param(param.name, x))
+        w.valueChanged[int].connect(lambda x: self.update_tool_param(param.name, x))
         return w
 
     def _convert_bool(self, param: xappt.Parameter) -> QtWidgets.QWidget:
         w = QtWidgets.QCheckBox()
-        if param.default is not None:
-            w.setChecked(param.default)
-        param.value = w.isChecked()
+        for v in (param.value, param.default):
+            if v is not None:
+                w.setChecked(v)
+                break
+        else:
+            param.value = w.isChecked()
         w.stateChanged.connect(lambda x: self.update_tool_param(param.name, x == QtCore.Qt.Checked))
         return w
 
     def _convert_str(self, param: xappt.Parameter) -> QtWidgets.QWidget:
         if param.choices is not None:
-            w = QtWidgets.QComboBox()
-            w.addItems(param.choices)
-            if param.default is not None and param.default in param.choices:
-                index = w.findText(param.default)
-                w.setCurrentIndex(index)
+            return self._convert_str_choice(param)
+        else:
+            return self._convert_str_edit(param)
+
+    def _convert_str_choice(self, param: xappt.Parameter) -> QtWidgets.QWidget:
+        w = QtWidgets.QComboBox()
+        w.addItems(param.choices)
+        for v in (param.value, param.default):
+            if v is not None:
+                if v in param.choices:
+                    index = w.findText(param.default)
+                    w.setCurrentIndex(index)
+                    break
+        else:
             param.value = w.currentText()
-            w.currentIndexChanged[str].connect(lambda x: self.update_tool_param(param.name, x))
+        w.currentIndexChanged[str].connect(lambda x: self.update_tool_param(param.name, x))
+        return w
+
+    def _convert_str_edit(self, param: xappt.Parameter) -> QtWidgets.QWidget:
+        ui = param.options.get("ui")
+        if ui == "folder-select":
+            w = FileEdit(mode=FileEdit.MODE_CHOOSE_DIR)
+            w.onSetFile.connect(lambda x: self.update_tool_param(param.name, x))
+        elif ui == "file-open":
+            w = FileEdit(accept=param.options.get("accept"), mode=FileEdit.MODE_OPEN_FILE)
+            w.onSetFile.connect(lambda x: self.update_tool_param(param.name, x))
+        elif ui == "file-save":
+            w = FileEdit(accept=param.options.get("accept"), mode=FileEdit.MODE_SAVE_FILE)
+            w.onSetFile.connect(lambda x: self.update_tool_param(param.name, x))
         else:
             w = QtWidgets.QLineEdit()
-            param.value = w.text()
             w.textChanged.connect(lambda x: self.update_tool_param(param.name, x))
+
+        for v in (param.value, param.default):
+            if v is not None:
+                w.setText(param.value)
+                break
+        else:
+            w.setText("")
         return w
 
     # noinspection DuplicatedCode

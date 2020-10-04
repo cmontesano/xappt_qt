83b521523a2dd04d51ceeb226e392c3327a0040a
Christopher Montesano <c.e.montesano@gmail.com>
2020-10-04 10:26:30 -0400

    support collections and filtering

--- xappt_qt/browser.py
+++ xappt_qt/browser.py
@@ -1,6 +1,8 @@
-import math
 import os
 import sys
+
+from collections import defaultdict
+from typing import DefaultDict, List, Type
 
 from PySide2 import QtWidgets, QtGui, QtCore
 
@@ -17,27 +19,67 @@ from xappt_qt.gui.resources import icons
 
 class XapptBrowser(QtWidgets.QMainWindow, Ui_Browser):
     ROLE_TOOL_CLASS = QtCore.Qt.UserRole + 1
+    ROLE_ITEM_TYPE = QtCore.Qt.UserRole + 2
+
+    ITEM_TYPE_COLLECTION = 0
+    ITEM_TYPE_TOOL = 1
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.setWindowIcon(QtGui.QIcon(":appicon"))
-        self.populate_tools()
         self.treeTools.setItemDelegate(SimpleItemDelegate())
-        self.treeTools.itemActivated.connect(self.item_activated)
-        self.treeTools.itemSelectionChanged.connect(self.selection_changed)
+
         self.interfaces = []
 
-    def populate_tools(self):
-        for tool_name, tool_class in xappt.registered_tools():
-            item = QtWidgets.QTreeWidgetItem()
-            item.setText(0, tool_name)
-            item.setData(0, self.ROLE_TOOL_CLASS, tool_class)
-            self.treeTools.addTopLevelItem(item)
-        self.treeTools.sortItems(0, QtCore.Qt.AscendingOrder)
+        self.populate_plugins()
+        self.connect_signals()
+
+    def connect_signals(self):
+        self.treeTools.itemActivated.connect(self.item_activated)
+        self.treeTools.itemSelectionChanged.connect(self.selection_changed)
+
+        self.txtSearch.textChanged.connect(self.on_filter_tools)
+        # noinspection PyAttributeOutsideInit
+        self.__txtSearch_keyPressEvent_orig = self.txtSearch.keyPressEvent
+        self.txtSearch.keyPressEvent = self._filter_key_press
+
+    def _create_collection_item(self, collection_name: str) -> QtWidgets.QTreeWidgetItem:
+        item = QtWidgets.QTreeWidgetItem()
+        item.setText(0, collection_name)
+        item.setData(0, self.ROLE_TOOL_CLASS, None)
+        item.setData(0, self.ROLE_ITEM_TYPE, self.ITEM_TYPE_COLLECTION)
+        return item
+
+    def _create_tool_item(self, tool_class: Type[xappt.BaseTool]) -> QtWidgets.QTreeWidgetItem:
+        item = QtWidgets.QTreeWidgetItem()
+        item.setText(0, tool_class.name())
+        item.setToolTip(0, tool_class.help())
+        item.setData(0, self.ROLE_TOOL_CLASS, tool_class)
+        item.setData(0, self.ROLE_ITEM_TYPE, self.ITEM_TYPE_TOOL)
+        return item
+
+    def populate_plugins(self):
+        self.treeTools.clear()
+        plugin_list: DefaultDict[str, List[Type[xappt.BaseTool]]] = defaultdict(list)
+
+        for _, plugin_class in xappt.plugin_manager.registered_tools():
+            collection = plugin_class.collection()
+            plugin_list[collection].append(plugin_class)
+
+        for collection in sorted(plugin_list.keys(), key=lambda x: x.lower()):
+            collection_item = self._create_collection_item(collection)
+            self.treeTools.insertTopLevelItem(self.treeTools.topLevelItemCount(), collection_item)
+            for plugin in sorted(plugin_list[collection], key=lambda x: x.name().lower()):
+                tool_item = self._create_tool_item(plugin)
+                collection_item.addChild(tool_item)
+            collection_item.setExpanded(True)
 
     def item_activated(self, item: QtWidgets.QTreeWidgetItem, column: int):
-        tool_class = item.data(0, self.ROLE_TOOL_CLASS)
+        item_type = item.data(column, self.ROLE_ITEM_TYPE)
+        if item_type != self.ITEM_TYPE_TOOL:
+            return
+        tool_class = item.data(column, self.ROLE_TOOL_CLASS)
         interface = xappt.get_interface()
         self.interfaces.append(interface)
         interface.invoke(tool_class())
@@ -46,13 +88,52 @@ class XapptBrowser(QtWidgets.QMainWindow, Ui_Browser):
         help_text = ""
         selected_items = self.treeTools.selectedItems()
         if len(selected_items):
-            tool_class = selected_items[0].data(0, self.ROLE_TOOL_CLASS)  # type: xappt.models.BaseTool
-            if len(tool_class.help()):
+            tool_class = selected_items[0].data(0, self.ROLE_TOOL_CLASS)  # type: xappt.BaseTool
+            if tool_class is not None and len(tool_class.help()):
                 help_text = f"{tool_class.name()}: {tool_class.help()}"
         self.labelHelp.setText(help_text)
 
     def closeEvent(self, event: QtGui.QCloseEvent):
         self.interfaces.clear()
+
+    def _filter_key_press(self, event: QtGui.QKeyEvent):
+        if event.key() == QtCore.Qt.Key_Escape:
+            self.txtSearch.clear()
+        self.__txtSearch_keyPressEvent_orig(event)
+
+    def on_filter_tools(self, text: str):
+        if len(text) == 0:
+            iterator = QtWidgets.QTreeWidgetItemIterator(self.treeTools, QtWidgets.QTreeWidgetItemIterator.All)
+            while iterator.value():
+                item = iterator.value()
+                item.setHidden(False)
+                iterator += 1
+            return
+        search_terms = [item.lower() for item in text.split(" ") if len(item)]
+        self._filter_branch(search_terms, self.treeTools.invisibleRootItem())
+
+    def _filter_branch(self, search_terms: List[str], parent: QtWidgets.QTreeWidgetItem) -> int:
+        visible_children = 0
+        for c in range(parent.childCount()):
+            child = parent.child(c)
+            item_type = child.data(0, self.ROLE_ITEM_TYPE)
+            if item_type == self.ITEM_TYPE_TOOL:
+                child_text = child.text(0).lower()
+                visible_children += 1
+                item_hidden = False
+                for term in search_terms:
+                    if term not in child_text:
+                        item_hidden = True
+                        break
+                child.setHidden(item_hidden)
+                if item_hidden:
+                    visible_children -= 1
+            elif item_type == self.ITEM_TYPE_COLLECTION:
+                visible_children += self._filter_branch(search_terms, child)
+            else:
+                raise NotImplementedError
+        parent.setHidden(visible_children == 0)
+        return visible_children
 
 
 def main(args) -> int:
--- xappt_qt/gui/ui/browser.py
+++ xappt_qt/gui/ui/browser.py
@@ -17,39 +17,57 @@ class Ui_Browser(object):
     def setupUi(self, Browser):
         if not Browser.objectName():
             Browser.setObjectName(u"Browser")
-        Browser.resize(484, 473)
+        Browser.resize(553, 709)
         self.centralwidget = QWidget(Browser)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
+        self.labelHelp = QLabel(self.centralwidget)
+        self.labelHelp.setObjectName(u"labelHelp")
+        self.labelHelp.setWordWrap(True)
+
+        self.gridLayout.addWidget(self.labelHelp, 3, 0, 1, 1)
+
         self.treeTools = QTreeWidget(self.centralwidget)
         __qtreewidgetitem = QTreeWidgetItem()
         __qtreewidgetitem.setText(0, u"1");
         self.treeTools.setHeaderItem(__qtreewidgetitem)
         self.treeTools.setObjectName(u"treeTools")
         self.treeTools.setAlternatingRowColors(True)
-        self.treeTools.setRootIsDecorated(False)
-        self.treeTools.setSortingEnabled(True)
+        self.treeTools.setRootIsDecorated(True)
+        self.treeTools.setSortingEnabled(False)
         self.treeTools.header().setVisible(False)
 
-        self.gridLayout.addWidget(self.treeTools, 0, 0, 1, 2)
+        self.gridLayout.addWidget(self.treeTools, 1, 0, 1, 1)
 
-        self.labelHelp = QLabel(self.centralwidget)
-        self.labelHelp.setObjectName(u"labelHelp")
-        self.labelHelp.setWordWrap(True)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.txtSearch = QLineEdit(self.centralwidget)
+        self.txtSearch.setObjectName(u"txtSearch")
 
-        self.gridLayout.addWidget(self.labelHelp, 1, 0, 1, 2)
+        self.horizontalLayout.addWidget(self.txtSearch)
+
+        self.btnClear = QToolButton(self.centralwidget)
+        self.btnClear.setObjectName(u"btnClear")
+
+        self.horizontalLayout.addWidget(self.btnClear)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
 
         Browser.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(Browser)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 484, 27))
+        self.menubar.setGeometry(QRect(0, 0, 553, 27))
         Browser.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(Browser)
         self.statusbar.setObjectName(u"statusbar")
         Browser.setStatusBar(self.statusbar)
+        QWidget.setTabOrder(self.txtSearch, self.btnClear)
+        QWidget.setTabOrder(self.btnClear, self.treeTools)
 
         self.retranslateUi(Browser)
+        self.btnClear.clicked.connect(self.txtSearch.clear)
 
         QMetaObject.connectSlotsByName(Browser)
     # setupUi
@@ -57,5 +75,7 @@ class Ui_Browser(object):
     def retranslateUi(self, Browser):
         Browser.setWindowTitle(QCoreApplication.translate("Browser", u"Xappt Browser", None))
         self.labelHelp.setText("")
+        self.txtSearch.setPlaceholderText(QCoreApplication.translate("Browser", u"Search", None))
+        self.btnClear.setText(QCoreApplication.translate("Browser", u"\u232b", None))
     # retranslateUi
 

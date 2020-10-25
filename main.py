8c2a219986434a1133e509aa8cf66a625d38310e
Christopher Montesano <c.e.montesano@gmail.com>
2020-10-24 21:59:54 -0400

    make single app entry point, record nuitka exe path

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import os
 import sys
 
+import xappt_qt
 import xappt_qt.browser
 import xappt_qt.launcher
 
 
+def main() -> int:
+    if len(sys.argv) == 1:
+        return xappt_qt.browser.entry_point()
+    else:
+        return xappt_qt.launcher.entry_point()
+
+
 if __name__ == '__main__':
-    sys.exit(xappt_qt.browser.entry_point())
+    if getattr(xappt_qt, "__compiled__", None) is not None:
+        xappt_qt.executable = os.path.abspath(sys.argv[0])
+    sys.exit(main())

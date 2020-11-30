4bebe8307b276512119658a6ad090b6adf5fe04c
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2020-11-30 12:50:40 -0800

    Pass along additional arguments for parameter initialization

--- xappt_qt/launcher.py
+++ xappt_qt/launcher.py
@@ -1,8 +1,12 @@
 import argparse
 import sys
 
+from typing import Dict, List, Type
+
 from PyQt5 import QtWidgets
+
 import xappt
+from xappt.cli import add_tool_args
 
 from xappt_qt.gui.utilities.dark_palette import apply_palette
 from xappt_qt.constants import *
@@ -11,6 +15,7 @@ from xappt_qt.constants import *
 def main(argv) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('toolname', help='Specify the name of the tool to load')
+    parser.add_argument('param_defaults', nargs=argparse.REMAINDER)
 
     options = parser.parse_args(args=argv)
 
@@ -26,10 +31,28 @@ def main(argv) -> int:
     app.setProperty(APP_PROPERTY_RUNNING, True)
 
     interface = xappt.get_interface()
-    tool_instance = tool_class(interface=interface)
+    params = params_from_args(tool_class=tool_class, args=options.param_defaults)
+    tool_instance = tool_class(interface=interface, **params)
     interface.invoke(tool_instance)
 
     return app.exec_()
+
+
+def params_from_args(tool_class: Type[xappt.BaseTool], args: List) -> Dict:
+    if not len(args):
+        return {}
+
+    tool_parser = argparse.ArgumentParser()
+    add_tool_args(parser=tool_parser, plugin_class=tool_class)
+    for action in tool_parser._actions:  # type: argparse.Action
+        action.required = False
+
+    tool_options, _ = tool_parser.parse_known_args(args=args)
+    params = vars(tool_options)
+    for key, value in list(params.items()):
+        if value is None:
+            params.pop(key)
+    return params
 
 
 def entry_point() -> int:

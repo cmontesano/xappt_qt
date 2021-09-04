194839a08fc3adf8862e87c5cead1ab3347156a6
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-04 07:26:31 -0400

    auto font size and preventing vertical resize

--- xappt_qt/gui/widgets/error_label.py
+++ xappt_qt/gui/widgets/error_label.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore, QtGui
 
 from xappt_qt.constants import *
 
@@ -9,13 +9,25 @@ class ErrorLabel(QtWidgets.QLabel):
         self._message: str = ""
         self.linkActivated.connect(self._on_link_activated)
 
+        font_size = QtGui.QFontDatabase.systemFont(QtGui.QFontDatabase.GeneralFont).pointSizeF()
+        mono_font = QtGui.QFontDatabase.systemFont(QtGui.QFontDatabase.FixedFont)
+        mono_font.setPointSizeF(font_size * 1.5)
+        self.setFont(mono_font)
+
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Ignored)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+        self.setSizePolicy(size_policy)
+
+        self.reset()
+
     def reset(self):
         self._message = ""
         self.setText("")
 
     def set_error(self, message: str):
         self._message = message
-        self.setText('<a href="#" style="color: red; text-decoration:none; font-size: 14pt;">⛔</a>')
+        self.setText('<a href="#" style="color: red; text-decoration:none;">⛔</a>')
 
     def show_error(self):
         if len(self._message):

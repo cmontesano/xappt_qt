cfe9e682b69250f2d1d60a41079c2b29803495d1
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-06 21:45:46 -0400

    new example - keeping ui open

--- xappt_qt/plugins/tools/examples/__init__.py
+++ xappt_qt/plugins/tools/examples/__init__.py
@@ -1,1 +1,2 @@
 from xappt_qt.plugins.tools.examples.video_convert import ConvertX265
+from xappt_qt.plugins.tools.examples.keep_interface_open import KeepInterfaceOpen
---
+++ xappt_qt/plugins/tools/examples/keep_interface_open.py
@@ -0,0 +1,41 @@
+import time
+
+import xappt
+
+
+@xappt.register_plugin
+class KeepInterfaceOpen(xappt.BaseTool):
+    iterations = xappt.ParamInt(options={"ui": "slider", "ticks": 1}, minimum=0, maximum=100, default=50,
+                                description="Pick a number between 1 and 100, or choose 0 to close the tool.")
+
+    @classmethod
+    def name(cls) -> str:
+        return "keep-ui-open"
+
+    @classmethod
+    def help(cls) -> str:
+        return "An example of keeping the Qt interface open. By default it will close after a tool finishes."
+
+    @classmethod
+    def collection(cls) -> str:
+        return "Examples"
+
+    def execute(self, *, interface: xappt.BaseInterface, **kwargs) -> int:
+        interface.progress_start()
+        iterations = self.iterations.value
+        for i in range(iterations):
+            progress = (i + 1) / iterations
+            interface.progress_update(f"Iteration: {i + 1}/{iterations}", progress)
+            time.sleep(0.05)
+        interface.progress_end()
+
+        interface.message("Complete")
+
+        if iterations > 0:
+            # queue another instance of this class
+            interface.add_tool(self.__class__)
+
+            # make sure the next instance is initialized with the same iterations as this instance
+            interface.tool_data['iterations'] = iterations
+
+        return 0

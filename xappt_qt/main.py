ce2d19a93fe4d4d49f9025ba2c5eb7a1f8192590
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-15 12:10:20 -0400

    ensure frozen exe is always discovered

--- xappt_qt/main.py
+++ xappt_qt/main.py
@@ -6,6 +6,12 @@ import xappt_qt.browser
 import xappt_qt.launcher
 
 
+if getattr(xappt_qt, "__compiled__", None) is not None:
+    xappt_qt.executable = os.path.abspath(sys.argv[0])
+elif getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+    xappt_qt.executable = os.path.abspath(sys.executable)
+
+
 def main() -> int:
     argc = len(sys.argv)
     if argc == 1:
@@ -15,8 +21,4 @@ def main() -> int:
 
 
 if __name__ == '__main__':
-    if getattr(xappt_qt, "__compiled__", None) is not None:
-        xappt_qt.executable = os.path.abspath(sys.argv[0])
-    elif getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
-        xappt_qt.executable = os.path.abspath(sys.executable)
     sys.exit(main())

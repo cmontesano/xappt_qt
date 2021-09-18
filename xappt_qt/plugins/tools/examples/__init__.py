21f02cb7036a1d80b4f93db5939b3599e2c0b30a
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-18 17:44:25 -0400

    default `auto_advance` to False

--- xappt_qt/plugins/tools/examples/__init__.py
+++ xappt_qt/plugins/tools/examples/__init__.py
@@ -1,4 +1,4 @@
 from xappt_qt.plugins.tools.examples.video_convert import ConvertX265
-from xappt_qt.plugins.tools.examples.keep_interface_open import KeepInterfaceOpen
 from xappt_qt.plugins.tools.examples.headless import HeadlessExecution
 from xappt_qt.plugins.tools.examples.custom_icon import *
+from xappt_qt.plugins.tools.examples.auto_advance import *

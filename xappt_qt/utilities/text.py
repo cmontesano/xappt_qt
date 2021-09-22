e62570dbf4fbfbee878c97376cbbb39869df56a4
Christopher Montesano <c.e.montesano@gmail.com>
2021-09-22 13:26:31 -0400

    move markdown wrapping to reusable method

---
+++ xappt_qt/utilities/text.py
@@ -0,0 +1,14 @@
+import markdown
+
+
+def to_markdown(text: str):
+    if len(text) == 0:
+        return text
+
+    md = markdown.markdown(text)
+    style = "".join((
+        "code {background-color: #000; color: #ccc;}",
+        "ul {margin-left: -20px;}",
+    ))
+    wrap = f"<html><head><style>{style}</style></head><body>{md}</body></html>"
+    return wrap

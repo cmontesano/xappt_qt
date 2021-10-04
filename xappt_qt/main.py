5c2dad7d7eab99089e5396d094530c5dd84ce5f3
Christopher Montesano <c.e.montesano@gmail.com>
2021-10-04 09:06:12 -0400

    better command line support

--- xappt_qt/main.py
+++ xappt_qt/main.py
@@ -1,9 +1,13 @@
+import argparse
 import os
 import sys
 
+import xappt
 import xappt_qt
 import xappt_qt.browser
 import xappt_qt.launcher
+
+from xappt_qt.constants import APP_INTERFACE_NAME
 
 
 if getattr(xappt_qt, "__compiled__", None) is not None:
@@ -13,11 +17,22 @@ elif getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
 
 
 def main() -> int:
-    argc = len(sys.argv)
-    if argc == 1:
-        return xappt_qt.browser.entry_point()
+    os.environ[xappt.INTERFACE_ENV] = APP_INTERFACE_NAME
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('toolname', type=str, help='Specify the name of the tool to load', nargs='?')
+    parser.add_argument('-v', '--version', action='store_true',
+                        help='Display the version number and build')
+
+    options, unknowns = parser.parse_known_args()
+
+    if options.version:
+        print(f"xappt_qt {xappt_qt.version_str}")
+
+    if options.toolname is None:
+        return xappt_qt.browser.main(sys.argv)
     else:
-        return xappt_qt.launcher.entry_point()
+        return xappt_qt.launcher.launch(options.toolname, unknown_args=unknowns)
 
 
 if __name__ == '__main__':
